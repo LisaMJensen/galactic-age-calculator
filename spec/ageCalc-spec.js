af1e79a928f8c3c294afff7e515d4e24b7178b80
Lisa Jensen <leese_38@yahoo.com>
2019-06-28 11:40:43 -0700

    add a bit more to functions - clearly don't understand Date object like I thought I did

--- spec/ageCalc-spec.js
+++ spec/ageCalc-spec.js
@@ -2,7 +2,7 @@
 describe (birthDate, function() {
 
   it (should test the input for year, month, and day) function() {
-    var birthday = new birthDate(1993, 8, 7);
+    var birthday = new Date(1993, 8, 7);
     expect(birthday.year).toEqual(1993);
     expect(birthday.month).toEqual(8);
     expect(birthday.day).toEqual(7);
@@ -11,6 +11,6 @@ describe (birthDate, function() {
 });
 
   it (should test mercuryAge returns the user's age on Mercury, function() {
-    var birthday = new birthDate (1991, 11, 20);
+    var birthday = new Date (1991, 11, 20);
     expect(birthday.mercuryAge()).toEqual(//I haven't written the function yet so how would I know?)
   }
