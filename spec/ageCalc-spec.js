566fca1d68ddcf7dee4eac716e8fcb66f43a9f3f
Lisa Jensen <leese_38@yahoo.com>
2019-06-28 10:31:30 -0700

    add a spec for mercuryAge function - going to start writing code

--- spec/ageCalc-spec.js
+++ spec/ageCalc-spec.js
@@ -9,3 +9,8 @@ describe (birthDate, function() {
 
   });
 });
+
+  it (should test mercuryAge returns the user's age on Mercury, function() {
+    var birthday = new birthDate (1991, 11, 20);
+    expect(birthday.mercuryAge()).toEqual(//I haven't written the function yet so how would I know?)
+  }
